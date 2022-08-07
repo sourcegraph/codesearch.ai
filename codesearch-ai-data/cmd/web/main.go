eec36e9f8173645787e0a59a8d40f7693e8b4101
Rok Novosel <rok@sourcegraph.com>
2022-08-07 13:52:10 +0200

    Add support for serving client in production.

--- codesearch-ai-data/cmd/web/main.go
+++ codesearch-ai-data/cmd/web/main.go
@@ -12,7 +12,6 @@ func main() {
 	isDevelopment := os.Getenv("DEVELOPMENT") == "true"
 
 	r := mux.NewRouter()
-	// API routes
 	if isDevelopment {
 		r.HandleFunc("/api/search/functions/by-text", mockSearchHandler("functions")).Methods("GET", "OPTIONS")
 		r.HandleFunc("/api/search/functions/by-code", mockSearchHandler("functions")).Methods("GET", "OPTIONS")
@@ -20,11 +19,17 @@ func main() {
 		r.HandleFunc("/api/search/so/by-text", mockSearchHandler("so")).Methods("GET", "OPTIONS")
 		r.HandleFunc("/api/search/so/by-code", mockSearchHandler("so")).Methods("GET", "OPTIONS")
 	} else {
+		// Static files
+		r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./client/build/static"))))
+
 		r.HandleFunc("/api/search/functions/by-text", searchFunctionsByTextHandler).Methods("GET", "OPTIONS")
 		r.HandleFunc("/api/search/functions/by-code", searchFunctionsByCodeHandler).Methods("GET", "OPTIONS")
 
 		r.HandleFunc("/api/search/so/by-text", searchSOByTextHandler).Methods("GET", "OPTIONS")
 		r.HandleFunc("/api/search/so/by-code", searchSOByCodeHandler).Methods("GET", "OPTIONS")
+
+		// Index
+		r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "client/build/index.html") }).Methods("GET")
 	}
 	http.Handle("/", r)
 
